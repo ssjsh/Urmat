56cb2623b7095b4d7acbc73c762d9fd4a47b93b7
MiamotoMusasi <urmatsh0608@gmail.com>
2023-04-23 19:09:42 +0600

    update

--- main.go
+++ main.go
@@ -7,15 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, World!")
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-
-func main2() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "From Miamoto")
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
 	http.ListenAndServe(":8080", nil)
